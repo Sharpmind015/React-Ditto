96bf7b3a1c2aba708c657de8827fdde03f6a9e0c
Akindeji <akindejioluwaseun015@gmail.com>
2020-10-17 19:32:11 -0700

    added the build folder

---
+++ build/precache-manifest.2f5e57260e23db2774a0df965b28aabc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801a3c6692754e0175700bc3ee49b527",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8010cd93f7a41b54f55",
+    "url": "./static/css/main.bdb58a60.chunk.css"
+  },
+  {
+    "revision": "a16913bbfbd3c8e5f2ff",
+    "url": "./static/js/2.e53a1f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e53a1f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8010cd93f7a41b54f55",
+    "url": "./static/js/main.e7651af1.chunk.js"
+  },
+  {
+    "revision": "e5a353c430b206cc5b3a",
+    "url": "./static/js/runtime-main.45ecce0e.js"
+  }
+]);
